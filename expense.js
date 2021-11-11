670b0ac89fc53870dce24db522ed002458db167c
Sam Shock <sam.shock924@gmail.com>
2021-11-10 22:34:18 -0500

    repo

---
+++ expense.js
@@ -0,0 +1,25 @@
+/* let's break this down my dude:
+first, select the date, the type of payment, a short description, and the cost of the payment.
+second, click submit to add the entry to the expense table
+the table will keep track of the above items, in descending order, with a total cost summary at the bottom (bonus)
+if you need to delete a payment entry, check on each entry you want deleted, and click the delete button at the bottom
+*/
+
+/* steps to get this puppy rolling:
+--get user input to appear in console.log
+--add row on submit with cells
+--get user input to each cell
+--checkbox on each expense
+--delete option at bottom of tracker
+--on an expense, red numbers (with a -)
+--on a credit, green numbers (with a +)
+--possibly add a "totals" tracker at bottom of tracker
+*/
+
+const expenseTable = document.querySelector('.expenseTable');
+const tableDate = document.querySelector('expenseTableDate');
+const tablePaymentType = document.querySelector('expenseTableType');
+const tableItemDescription = document.querySelector('expenseTableDescription');
+const tableItemCost = document.querySelector('expenseTableCost');
+const expenseRow = document.querySelector('expenseInfoRow');
+
