1a28b0ce7e59d3561b8a737c474bbe71eea829d3
Sam Shock <sam.shock924@gmail.com>
2021-12-13 20:54:40 -0500

    basics work, needs delete

--- expense.js
+++ expense.js
@@ -1,28 +1,4 @@
-/* let's break this down my dude:
-first, select the date, the type of payment, a short description, and the cost of the payment.
-second, click submit to add the entry to the expense table
-the table will keep track of the above items, in descending order, with a total cost summary at the bottom (bonus)
-if you need to delete a payment entry, check on each entry you want deleted, and click the delete button at the bottom
-*/
-
-/* steps to get this puppy rolling:
---get user input to appear in console.log
---add row on submit with cells
---get user input to each cell
---checkbox on each expense
---delete option at bottom of tracker
---on an expense, red numbers (with a -)
---on a credit, green numbers (with a +)
---possibly add a "totals" tracker at bottom of tracker
-*/
-
-// const expenseTable = document.querySelector('.expenseTable');
-// const tableDate = document.querySelector('#expenseTableDate');
-// const tablePaymentType = document.querySelector('#expenseTableType');
-// const tableItemDescription = document.querySelector('#expenseTableDescription');
-// const tableItemCost = document.querySelector('#expenseTableCost');
-// const expenseRow = document.querySelector('expenseInfoRow');
-
+const expenseTable = document.querySelector('.expenseTable');
 const submitButton = document.querySelector('#submitButton');
 const expenseForm = document.querySelector('#expenseInfoForm');
 const formDate = document.querySelector('#expenseInfoDate');
@@ -30,11 +6,21 @@ const formPaymentOption = document.querySelector('#expenseInfoType');
 const formDescription = document.querySelector('#expenseInfoDescription');
 const formCost = document.querySelector('#expenseInfoCost');
 
-submitButton.addEventListener('click', getInput)
+submitButton.addEventListener('click', e => {
+    e.preventDefault();
+    addRow();
+});
 
-function getInput(){
-    console.log(formDate.value);
-    console.log(formPaymentOption.value);
-    console.log(formDescription.value);
-    console.log(formCost.value);
-};
+function addRow(){
+    let newRow = expenseTable.insertRow(1);
+    let dateCell = newRow.insertCell(0);
+    let paymentTypeCell = newRow.insertCell(1);
+    let descriptionCell = newRow.insertCell(2);
+    let costCell = newRow.insertCell(3);
+
+    dateCell.innerHTML = formDate.value;
+    paymentTypeCell.innerHTML = formPaymentOption.value;
+    descriptionCell.innerHTML = formDescription.value;
+    costCell.innerHTML = formCost.value;
+}
+
