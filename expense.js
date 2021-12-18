1b46ed9db6e17a4c6fe0937da5adf7562b6cac86
Sam Shock <sam.shock924@gmail.com>
2021-12-17 19:23:08 -0500

    working on delete button

--- expense.js
+++ expense.js
@@ -5,10 +5,20 @@ const formDate = document.querySelector('#expenseInfoDate');
 const formPaymentOption = document.querySelector('#expenseInfoType');
 const formDescription = document.querySelector('#expenseInfoDescription');
 const formCost = document.querySelector('#expenseInfoCost');
+const deleteButton = document.createElement("button");
+
+
+function addDeleteButton(){
+    deleteButton.setAttribute('class', 'deleteButton');
+    deleteButton.textContent = 'Delete';
+    expenseTable.appendChild(deleteButton);
+}
+
 
 submitButton.addEventListener('click', e => {
     e.preventDefault();
     addRow();
+    addDeleteButton();
 });
 
 function addRow(){
@@ -17,10 +27,12 @@ function addRow(){
     let paymentTypeCell = newRow.insertCell(1);
     let descriptionCell = newRow.insertCell(2);
     let costCell = newRow.insertCell(3);
+    let deleteButtonCell = newRow.insertCell(4);
 
     dateCell.innerHTML = formDate.value;
     paymentTypeCell.innerHTML = formPaymentOption.value;
     descriptionCell.innerHTML = formDescription.value;
-    costCell.innerHTML = formCost.value;
+    costCell.innerHTML = "$" + formCost.value;
+    
 }
 
