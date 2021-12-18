7104bc8308ba91fcd185bf810234eab79cccc70e
Sam Shock <sam.shock924@gmail.com>
2021-12-18 03:15:29 -0500

    delete button appears, just need row delete

--- expense.js
+++ expense.js
@@ -5,34 +5,36 @@ const formDate = document.querySelector('#expenseInfoDate');
 const formPaymentOption = document.querySelector('#expenseInfoType');
 const formDescription = document.querySelector('#expenseInfoDescription');
 const formCost = document.querySelector('#expenseInfoCost');
-const deleteButton = document.createElement("button");
+const expenseRow = document.querySelector('#expenseInfoRow');
 
 
 function addDeleteButton(){
+    const deleteButton = document.createElement("button");
     deleteButton.setAttribute('class', 'deleteButton');
     deleteButton.textContent = 'Delete';
-    expenseTable.appendChild(deleteButton);
+    return deleteButton;
 }
 
 
 submitButton.addEventListener('click', e => {
     e.preventDefault();
     addRow();
-    addDeleteButton();
 });
 
 function addRow(){
-    let newRow = expenseTable.insertRow(1);
-    let dateCell = newRow.insertCell(0);
-    let paymentTypeCell = newRow.insertCell(1);
-    let descriptionCell = newRow.insertCell(2);
-    let costCell = newRow.insertCell(3);
-    let deleteButtonCell = newRow.insertCell(4);
+    const newRow = expenseTable.insertRow(1);
+    const dateCell = newRow.insertCell(0);
+    const paymentTypeCell = newRow.insertCell(1);
+    const descriptionCell = newRow.insertCell(2);
+    const costCell = newRow.insertCell(3);
+    const deleteButtonCell = newRow.insertCell(4);
+
+    const deleteButton = addDeleteButton();
 
     dateCell.innerHTML = formDate.value;
     paymentTypeCell.innerHTML = formPaymentOption.value;
     descriptionCell.innerHTML = formDescription.value;
     costCell.innerHTML = "$" + formCost.value;
-    
+    deleteButtonCell.appendChild(deleteButton);
 }
 
